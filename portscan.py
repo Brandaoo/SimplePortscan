9bf5c56d4bd52cd669a3263c6298ec016c79b282
Gabriel brandao da cruz runa <93392038+Brandaoo@users.noreply.github.com>
2021-10-29 23:04:27 +0200

    Add files via upload

---
+++ portscan.py
@@ -0,0 +1,28 @@
+import socket
+import pyfiglet
+
+
+
+
+ascii_banner = pyfiglet.figlet_format("Portscan")
+print(ascii_banner)
+
+
+ip = input("ip adress: ")
+
+for port in range(1, 65535):
+
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.settimeout(0.2)
+
+	if s.connect_ex((ip,port)) ==0:
+
+
+		print(ip, "====>" , port, "is open")
+
+
+
+
+
+
+
